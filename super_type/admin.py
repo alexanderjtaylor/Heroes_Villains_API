4447a7800e2274eba3f189e5c2c0d2f43c13350f
Alex <alexandertaylor315@gmail.com>
2022-10-23 23:10:16 -0400

    added super_type and supers app + updated installed_apps in settings

---
+++ super_type/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Super_Type
+admin.site.register(Super_Type)
+
+# Register your models here.
---
+++ supers/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+from super_type.models import Super_Type
+
+# Create your models here.
+
+class Super(models.Model):
+    name = models.CharField(max_length = 255)
+    alter_ego = models.CharField(max_length = 255) 
+    primary_ability = models.CharField(max_length = 255)
+    secondary_ability = models.CharField(max_length = 255)
+    catchphrase = models.CharField(max_length = 255)
+    super_type = models.ForeignKey(Super_Type, on_delete = models.CASCADE)
