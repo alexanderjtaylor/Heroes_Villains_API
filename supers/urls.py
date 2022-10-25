a7bb7abd4023bbd41bc9db530a2f8df5b6b7b364
Alex <alexandertaylor315@gmail.com>
2022-10-25 00:04:33 -0400

    registered supers urls.py to project urls.py + updated views.py in supers app to execute postman requests

--- supers/urls.py
+++ supers/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from supers import views
 
 urlpatterns = [
-    path('api/supers/', views.supers_list),
-    path('api/supers/<int:pk>', views.super.details)
+    path('', views.supers_list),
 ]
