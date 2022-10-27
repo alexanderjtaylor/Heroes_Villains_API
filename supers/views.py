e6cb157ece8ebb9ea10fcd3f19898ccef93ddf1e
Alex <alexandertaylor315@gmail.com>
2022-10-26 21:09:24 -0400

    updated functionality for POST, PUT, DELETE in supers views.py

--- supers/views.py
+++ supers/views.py
@@ -9,7 +9,7 @@ from supers import serializers
 
 # Create your views here.
 
-@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+@api_view(['GET', 'POST'])
 def supers_list(request):
     
     if request.method == 'GET':
@@ -23,32 +23,18 @@ def supers_list(request):
         serializer.save()
         return Response(serializer.data, status = status.HTTP_201_CREATED)
     
-    elif request.method == 'PUT':
-        serializer = SupersSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status = status.HTTP_200_OK)
-    
-    elif request.method == 'DELETE':
-        serializer = SupersSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status = status.HTTP_204_NO_CONTENT)
-    
 @api_view(['GET', 'PUT', 'DELETE'])
 def supers_details(request, pk):
-    super = get_object_or_404(Supers, pk=pk)
+    supers = get_object_or_404(Supers, pk=pk)
     if request.method == 'GET':
-        serializer = SupersSerializer(super)
+        serializer = SupersSerializer(supers)
         return Response(serializer.data)
     elif request.method == 'PUT':
-        serializer = SupersSerializer(super)
+        serializer = SupersSerializer(supers, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
     elif request.method == 'DELETE':
-        serializer = SupersSerializer(super)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status = status.HTTP_204_NO_CONTENT)
+        supers.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
         
